3be37636fc144773309dc0959ebac0cbbe885069
Italo <italokun@gmail.com>
2024-11-28 21:06:43 -0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   img/img06.png
    	modified:   package/__pycache__/configuracoes.cpython-312.pyc
    	modified:   package/__pycache__/funcoes_jb.cpython-312.pyc
    	modified:   package/__pycache__/imagens.cpython-312.pyc
    	modified:   postagens/02.json
    	modified:   postagens/03.json
    	modified:   postagens/08.json
    	modified:   postagens/13.json
    	modified:   postagens/14.json
    	modified:   postagens/15.json
    	modified:   postagens/17.json
    	modified:   postagens/19.json
    	modified:   postagens/20.json
    	modified:   postagens/22.json
    	modified:   postagens/25.json
    	modified:   postagens/26.json
    	modified:   postagens/27.json
    	modified:   postagens/28.json
    	modified:   postagens/31.json
    	modified:   postagens/32.json
    	modified:   postagens/35.json
    	modified:   postagens/37.json
    	modified:   postagens/40.json
    	modified:   postagens/41.json
    	modified:   postagens/42.json
    	modified:   postagens/43.json
    	modified:   postagens/44.json
    	modified:   postagens/45.json
    	modified:   postagens/46.json
    	modified:   postagens/52.json
    	modified:   postagens/53.json
    	modified:   postagens/54.json
    	modified:   postagens/55.json
    	modified:   postagens/56.json
    	modified:   postagens/57.json
    	modified:   postagens/58.json
    	modified:   postagens/59.json
    	modified:   postagens/60.json
    	modified:   publicador_jm.py

--- publicador_jm.py
+++ publicador_jm.py
@@ -5,20 +5,20 @@ from time import sleep
 lojas = get_tokens()
 img = selecionar_imagem()
 
-# for loja in lojas.keys():
-#     try:
-#         token = lojas.get(loja)
-#         publicar_post(loja,img,token[0])
-#     except KeyError:
-#         print(f'Página {loja} {token[1]} atualizou o token.')
-#         sleep(10)
-#         lojas = get_tokens()
-#         token = lojas.get(loja)
-#         print('Novos Tokens foram carregados com sucesso.')
-#         publicar_post(loja,img,token[0])
-#     except FileNotFoundError:
-#         print('Imagem ou Mensagem não encontrada, consulte o Log de Erros.')
-#         break
+for loja in lojas.keys():
+    try:
+        token = lojas.get(loja)
+        publicar_post(loja,img,token[0])
+    except KeyError:
+        print(f'Página {loja} {token[1]} atualizou o token.')
+        sleep(10)
+        lojas = get_tokens()
+        token = lojas.get(loja)
+        print('Novos Tokens foram carregados com sucesso.')
+        publicar_post(loja,img,token[0])
+    except FileNotFoundError:
+        print('Imagem ou Mensagem não encontrada, consulte o Log de Erros.')
+        break
     
 
 # for loja in lojas.keys():
