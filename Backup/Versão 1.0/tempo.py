0679fcf2bf46639e7f0e10c1746e009e0d178ca8
Italo <italokun@gmail.com>
2025-01-16 11:14:37 -0300

    removing Backup

--- Backup/Versão 1.0/tempo.py
+++
@@ -1,7 +0,0 @@
-from datetime import date
-from datetime import datetime
-
-hora = datetime.today()
-dia = date.today()
-
-print(f'03 Token Inválido - {hora}\n')
--- Backup/caminho.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-p = Path().resolve()
-
-print(p)
